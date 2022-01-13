7e5c2d080de79c086569bdc66ae96264108940d5
Kehinde <kehinde.alade@patricia.com.ng>
2022-01-13 13:23:40 +0100

    Added dashboard logic

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -9,23 +9,26 @@ use App\Models\CounselRequest;
 use Illuminate\Support\Facades\Auth;
 
 class BaseController extends Controller
-{   
-
-
+{
     //Redirect method defined here
-    public function redirect(){
-        if(Auth::user()->usertype == "councilor")
-           { 
-            
-            $category = Category::firstWhere('user_id', Auth::user()->id);
-            return view('admin/dashboard', compact('category'));
-           }
-           else if(Auth::user()->usertype == "councilee")
-           {
-            $counsellors = Category::all();
-            $counselle = User::firstWhere('id', Auth::user()->id );
-            return view('/dashboard', compact('counsellors', 'counselle'));
-           }
+    public function redirect()
+    {
+        if (Auth::user()->usertype == "councilor") {
 
+            $category = Category::firstWhere('user_id', Auth::user()->id);
+            $counselRequests = [];
+
+            if (!is_null($category)) {
+                $counselRequests = CounselRequest::where('category_id', $category->id)->get();
+            }
+
+            return view('admin/dashboard', compact('category', 'counselRequests'));
+        } else if (Auth::user()->usertype == "councilee") {
+            $counsellors = Category::all(); //Name this variable accordingly
+            $counselle = User::firstWhere('id', Auth::user()->id);
+            $counsellorss = User::where('usertype', 'councilor')->get();
+            
+            return view('/dashboard', compact('counsellors', 'counselle'));
+        }
     }
 }
